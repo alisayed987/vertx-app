2906b05cf8f1c4764e4995d17c9b8061d3ccf9cd
alisayed987 <ali.sayed987@yahoo.com>
2022-01-15 16:24:46 +0200

    verticle communication

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -3,6 +3,12 @@ package com.example.starter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainVerticle extends AbstractVerticle {
 
   @Override
@@ -19,5 +25,45 @@ public class MainVerticle extends AbstractVerticle {
         startPromise.fail(http.cause());
       }
     });
+
+    vertx.deployVerticle(new Verticle2());
+    Float var = getEGP("https://freecurrencyapi.net/api/v2/latest?apikey=c604bf70-746f-11ec-a4ee-d7b783de69df");
+    vertx.eventBus().request("recieve.address",var,handler ->{
+      System.out.println("dollars to bounds in Main V :  "+handler.result().body());
+    });
+  }
+
+  public float getEGP(String myUrl) {
+    try {
+      // Http GET request
+      URL url = new URL(myUrl);
+      HttpURLConnection con = (HttpURLConnection) url.openConnection();
+      con.setRequestMethod("GET");
+      con.setRequestProperty("Accept", "application/json");
+      InputStream responseStream = con.getInputStream();
+      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+      String inputline;
+      StringBuffer response = new StringBuffer();
+
+      // appending response strings together
+      while((inputline = in.readLine()) != null){
+        response.append(inputline);
+      }
+      System.out.println("tst");
+      int start = response.toString().indexOf("EGP");
+      String stri = response.toString().substring(start+5,start+10);
+      System.out.println(stri);
+      // Extracting EGP from the String Response
+
+      Float EGP = Float.parseFloat( stri ) ;
+      System.out.println(EGP);
+
+      return (EGP);
+    } catch (Exception e) {
+      e.printStackTrace();
+
+      return 0;
+    }
+
   }
 }
